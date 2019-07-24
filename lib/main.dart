6cde0e255c3b2dd02d48683fb8ab1a5ee449244f
Wallison Rocha <wallison.rocha@webgoal.com.br>
2019-07-24 17:39:14 -0300

    Image, Padding e Stack

--- lib/main.dart
+++ lib/main.dart
@@ -2,51 +2,66 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
     title: "Contador de Pessoas",
-    home: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-                Text(
-                  "Pessoas 0",
-                  style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      color: Colors.white
+    home: Stack(
+      children: <Widget>[
+        Image.asset(
+          "images/bg.png",
+          fit: BoxFit.cover,
+          height: 1000.0,
+        ),
+        Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              "Pessoas 0",
+              style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Padding(
+                  padding: EdgeInsets.all(10.0),
+                  child: FlatButton(
+                      onPressed: () {},
+                      child: Text(
+                          "+1",
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontStyle: FontStyle.italic,
+                              fontSize: 40.0)
+                      )
                   ),
                 ),
-               Row(
-                 mainAxisAlignment: MainAxisAlignment.center,
-                 children: <Widget>[
-                   FlatButton(
-                       onPressed: () {},
-                       child: Text(
-                         "+1",
-                         style: TextStyle(
-                             color: Colors.white,
-                             fontStyle: FontStyle.italic,
-                             fontSize: 40.0)
-                       )
-                   ),
-                   FlatButton(
-                       onPressed: () {},
-                       child: Text(
-                           "-1",
-                           style: TextStyle(
-                               color: Colors.white,
-                               fontStyle: FontStyle.italic,
-                               fontSize: 40.0)
-                       )
-                   ),
-                 ],
-               ),
-               Text(
-                 "Pode entrar",
-                 style: TextStyle(
-                     color: Colors.white,
-                     fontStyle: FontStyle.italic,
-                     fontSize: 30.0
-                 ),
-               )
-            ],
-    
-          ),
+                Padding(
+                  padding: EdgeInsets.all(10.0),
+                  child: FlatButton(
+                      onPressed: () {},
+                      child: Text(
+                          "-1",
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontStyle: FontStyle.italic,
+                              fontSize: 40.0)
+                      )
+                  ),
+                ),
+              ],
+            ),
+            Text(
+              "Pode entrar",
+              style: TextStyle(
+                  color: Colors.white,
+                  fontStyle: FontStyle.italic,
+                  fontSize: 30.0
+              ),
+            )
+          ],
+
+        ),
+      ],
+    )
       
 ));
